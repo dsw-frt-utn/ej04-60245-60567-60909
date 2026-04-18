a79fcd077b46ede672d481626272b38af29b8d21
Leal <lautarolealalvarado10@gmail.com>
2026-04-18 19:04:33 -0300

    Confirmado cambios

--- src/main/java/data/Persistencia.java
+++ src/main/java/data/Persistencia.java
@@ -8,6 +8,7 @@ public class Persistencia {
     private static ArrayList<Vehiculo> vehiculos = new ArrayList<>();
     private static ArrayList<Responsable> responsables = new ArrayList<>();
     private static ArrayList<Sucursal> sucursales = new ArrayList<>();
+    private static ArrayList<Marca> marcas = new ArrayList<>();
     
     private static void inicializarResponsables(){
         Responsable r1 = new Responsable("Carlos Gómez", "25444111", "3815551111");
@@ -24,20 +25,24 @@ public class Persistencia {
         sucursales.add(s2);
     }
     
-    private static void inicializarVehiculos(){
-        Sucursal s1 = sucursales.get(0);
-        Sucursal s2 = sucursales.get(1);
-        
-        VehiculoElectrico v1 = new VehiculoElectrico("AE123FG", "Renault", "Kangoo E-Tech", 2020, 1000, s1, 16);
-        VehiculoElectrico v2 = new VehiculoElectrico("AF456HI", "Ford", "E-Transit", 2021, 1300, s2, 16);
-
-        VehiculoCombustible v3 = new VehiculoCombustible("AC789JK", "Iveco", "Daily", 2023, 1200, s1, 8, 1.5);
-        VehiculoCombustible v4 = new VehiculoCombustible("AD321LM", "Mercedes", "Sprinter", 2020, 1200, s2, 7, 1);
-        
-        vehiculos.add(v1);
-        vehiculos.add(v2);
-        vehiculos.add(v3);
-        vehiculos.add(v4);
+    public static ArrayList<Sucursal> getSucursales() { //Getter nuevo de ventana
+        return sucursales;
+    }
+    
+    private static void inicializarMarcas() { //Teniendo clase Marca ya incluida
+        marcas.add(new Marca("Renault"));
+        marcas.add(new Marca("Ford"));
+        marcas.add(new Marca("Iveco"));
+        marcas.add(new Marca("Mercedes"));
+        marcas.add(new Marca("Toyota"));
+        marcas.add(new Marca("Volkswagen"));
+    }
+    public static ArrayList<Marca> getMarcas() {
+        return marcas;
+    }
+    
+    public static void agregarVehiculo(Vehiculo vehiculo) { //Nuevo para guardarr
+        vehiculos.add(vehiculo);
     }
     
     public static ArrayList<Vehiculo> getVehiculos(){
@@ -54,5 +59,20 @@ public class Persistencia {
         inicializarResponsables();
         inicializarSucursales();
         inicializarVehiculos();
+        inicializarMarcas();
     }
+    
+    private static void inicializarVehiculos(){
+        /*Sucursal s1 = sucursales.get(0);
+        Sucursal s2 = sucursales.get(1);
+        VehiculoElectrico v1 = new VehiculoElectrico("AE123FG", "Renault", "Kangoo E-Tech", 2020, 1000, s1, 16);
+        VehiculoElectrico v2 = new VehiculoElectrico("AF456HI", "Ford", "E-Transit", 2021, 1300, s2, 16);
+        VehiculoCombustible v3 = new VehiculoCombustible("AC789JK", "Iveco", "Daily", 2023, 1200, s1, 8, 1.5);
+        VehiculoCombustible v4 = new VehiculoCombustible("AD321LM", "Mercedes", "Sprinter", 2020, 1200, s2, 7, 1);
+        vehiculos.add(v1);
+        vehiculos.add(v2);
+        vehiculos.add(v3);
+        vehiculos.add(v4);*/
+    }
+    
 }
--- src/main/java/views/Controlador.java
+++ src/main/java/views/Controlador.java
@@ -3,6 +3,7 @@ package views;
 import data.Persistencia;
 import domain.Vehiculo;
 import domain.VehiculoTipo;
+import domain.*;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
@@ -31,4 +32,23 @@ public class Controlador {
         }
         return new double[] {consumoElectricos, consumoCombustible};
     }
+    
+    
+    public static void registrarNuevoVehiculo(domain.VehiculoTipo tipo, String patente, domain.Marca marca, 
+                                          String modelo, int anio, double capacidadCarga, 
+                                          domain.Sucursal sucursal, double p1, double p2) {
+    
+    domain.Vehiculo nuevo;
+    
+    if (tipo == domain.VehiculoTipo.ELECTRICO) {
+        // p1 es kwhBase
+        nuevo = new domain.VehiculoElectrico(patente, marca, modelo, anio, capacidadCarga, sucursal, p1);
+    } else {
+        // p1 es kmPorLitro, p2 es litrosExtra
+        nuevo = new domain.VehiculoCombustible(patente, marca, modelo, anio, capacidadCarga, sucursal, p1, p2);
+    }
+    
+    data.Persistencia.agregarVehiculo(nuevo);
+}
+    
 }
