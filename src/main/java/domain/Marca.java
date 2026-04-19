eb7664088edf86c3eb5fc593ce7ca91b4c0dcff4
77Man <SamirFarhat@gmail.com>
2026-04-18 22:08:38 -0300

    Incorporacion clase marca+

---
+++ src/main/java/domain/Marca.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Marca {
+    private String nombre;
+
+    public Marca(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
+
--- src/main/java/domain/VehiculoCombustible.java
+++ src/main/java/domain/VehiculoCombustible.java
@@ -1,7 +1,4 @@
-
 package domain;
-
-import java.time.Year;
 
 import java.time.Year;
 
@@ -9,7 +6,7 @@ public class VehiculoCombustible extends Vehiculo {
     private double kilometrosPorLitro;
     private double litrosExtra;
 
-    public VehiculoCombustible(String patente, String marca, String modelo, int anio, double capacidadCarga,
+    public VehiculoCombustible(String patente, Marca marca, String modelo, int anio, double capacidadCarga,
                                Sucursal sucursal, double kilometrosPorLitro, double litrosExtra) {
         super(VehiculoTipo.COMBUSTIBLE, patente, marca, modelo, anio, capacidadCarga, sucursal);
         this.kilometrosPorLitro = kilometrosPorLitro;
